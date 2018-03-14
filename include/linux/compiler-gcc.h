86b8ec470d9942cd128163693059774db476ed17
madScript <madscript01official@gmail.com>
2018-03-14 21:26:46 +0000

    Revert "Fixed Compiler"
    This reverts commit b83baf7a1b50c28887aeface7baa464e2b2cad5b.

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -103,6 +103,7 @@
 #define __gcc_header(x) #x
 #define _gcc_header(x) __gcc_header(linux/compiler-gcc##x.h)
 #define gcc_header(x) _gcc_header(x)
+#include gcc_header(__GNUC__)
 
 #if !defined(__noclone)
 #define __noclone	/* not needed */
