e4bc17360dd770967fd95949fe01f2efeeba27f4
madScript <madscript01official@gmail.com>
2018-03-17 12:30:32 +0000

    Updated

--- dtbgen.sh
+++ dtbgen.sh
@@ -5,7 +5,7 @@
 RDIR=$(pwd)
 
 # directory containing cross-compile arm64 toolchain
-TOOLCHAIN=/home/madscript/Github/KernelBuilder_Pack/Toolchains/gcc-linaro-7.2.1-2017.11-i686_aarch64-linux-gnu
+TOOLCHAIN=/home/madscript/Github/KernelBuilder_Pack/Toolchains/gcc-linaro-4.9.4-2017.01-i686_aarch64-linux-gnu
 
 # device dependant variables
 PAGE_SIZE=2048
